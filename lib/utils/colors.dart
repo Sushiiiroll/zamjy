b2b9667686a861b3c4ae5a46327f0bd4e3f1ff5c
Warren Virgines <xwarrenvirgines@gmail.com>
2023-07-19 10:15:54 +0800

    Fixed Account UI

---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static Color elevatedButtonColor = const Color(0xff007B74);
+}
---
+++ lib/utils/images.dart
@@ -0,0 +1,9 @@
+class Assets {
+  static String foodPath = "assets/images/";
+
+  static String bibimbap = "${foodPath}bibimbap.png";
+  static String bulgogi = "${foodPath}bulgogi.png";
+  static String sisig = "${foodPath}sisig.png";
+  static String ramen = "${foodPath}ramen.png";
+  static String cart = "${foodPath}cart.png";
+}
